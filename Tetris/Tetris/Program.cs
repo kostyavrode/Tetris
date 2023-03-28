bdef280b9b3d5e4ad9bdd804de51bad5909fa7b0
kostyavrode <inbloomf3@gmail.com>
2023-03-28 17:55:21 +0300

    lesson2

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -10,6 +10,24 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
+
+            
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '^';
+
+
+            Drow(x1, y1, c1);
+            Console.ReadLine();
+        }
+        static void Drow(int x,int y,char symbol)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+
         }
     }
 }
